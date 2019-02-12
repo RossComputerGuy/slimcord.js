c15a9e21ab25ff1042e408c73e51ffe2a71dff0e
SpaceboyRoss01 <spaceboyross@yandex.com>
2019-02-12 12:00:35 -0800

    Better file structure, new guild module

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require('./src/');
+module.exports = {
+  Core: require('./src/core.js'),
+  Module: require('./src/module.js'),
+  modules: {
+    GuildModule: require('./src/modules/guild.js')
+  }
+};
+
+// vim:set ts=2 sw=2 et:
---
+++ src/modules/guild.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js'):
+const Module = require('../module.js');
+
+class Guild {
+  constructor(core, guild) {
+    this.core = core;
+    this.guild = guild;
+  }
+}
+
+class GuildModule extends Module {
+  metadata() {
+    return {
+      name: 'Guild Utilties',
+      desc: 'Provides helping functions to manage and find guilds.',
+      author: 'Spaceboy Ross',
+      ver: '0.1.0',
+      url: 'https://github.com/SpaceboyRoss01/slimcord.js',
+      provides: [
+        'slimbot.js/guilds'
+      ]
+    };
+  }
+  async init() {
+    this.core.singleton('slimbot.js/guilds', () => ({
+      findByKey: (key, value) => new Guild(this.core, this.core.client.guilds.find(key, value)),
+      getGuilds: () => this.core.client.guilds.array().map(guild => new Guild(this.core, guild))
+    }));
+  }
+  start() {
+  }
+  destroy() {
+  }
+}
+module.exports = GuildModule;
+
+// vim:set ts=2 sw=2 et:
